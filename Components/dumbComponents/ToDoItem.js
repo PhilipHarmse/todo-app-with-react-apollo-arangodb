678ccff58d51c0939d36e3e9ae498f78978ec8be
Philip Harmse <philip.harmse@gmail.com>
2016-08-22 20:35:36 +0200

    improved apearance todo modified buttons

--- Components/dumbComponents/ToDoItem.js
+++ Components/dumbComponents/ToDoItem.js
@@ -3,25 +3,27 @@ import React from 'react';
 
 
 
-export default class ToDoItem extends React.Component{
-    handleDeleteEvent(){        
+export default class ToDoItem extends React.Component {
+    handleDeleteEvent() {
         this.props.deleteToDo(this.props.item.id);
     }
 
-    handleEditEvent(){
+    handleEditEvent() {
         this.props.setToDoInEditMode(this.props.item.id)
     }
 
-    render(){
-        return(            
-                <tr>
-                    <td> {this.props.item.name}</td>
-                    <td> {this.props.item.status}</td>
-                    <td>
-                        <button onClick={this.handleDeleteEvent.bind(this)} class="btn btn-sm btn-danger">delete</button> 
-                        <button onClick={this.handleEditEvent.bind(this)} class="btn btn-sm btn-danger">edit</button>
-                    </td>
-                </tr>                               
+    render() {
+        return (
+            <tr>
+                <td> {this.props.item.name}</td>
+                <td> {this.props.item.status}</td>
+                <td>
+                    <div class="btn-group">
+                        <button type="button" onClick={this.handleDeleteEvent.bind(this) } class="btn btn-sm btn-danger">delete </button><span> </span>
+                        <button type="button" onClick={this.handleEditEvent.bind(this) } class="btn btn-sm btn-info">edit</button>
+                    </div>
+                </td>
+            </tr>
         )
     }
 }
--- Components/dumbComponents/ToDoItemInEditMode.js
+++ Components/dumbComponents/ToDoItemInEditMode.js
@@ -4,34 +4,35 @@ import React from 'react';
 
 
 
-export default class ToDoItemInEditMode extends React.Component{
-    handleDeleteEvent(){        
+export default class ToDoItemInEditMode extends React.Component {
+    handleDeleteEvent() {
         this.props.deleteToDo(this.props.item.id);
     }
 
-    handleEditEvent(){
+    handleEditEvent() {
         this.props.editToDo(this.props.item.id)
     }
 
-    handleChange(e){
+    handleChange(e) {
         this.props.addToEditToDo(e.target.value)
     }
 
-    render(){
-        return(            
-                <tr>
-                    <td>
-                        <div class = "form-group">                            
-                            <input onChange={this.handleChange.bind(this)} class="form-control" value ={this.props.partialEditTodo}/>
-                        </div>
-                    </td>
-                    <td> {this.props.item.status}</td>
-                    <td>
-                    
-                        <button type="button" onClick={this.handleDeleteEvent.bind(this)} class="btn btn-sm btn-danger">delete</button><span> </span>
-                        <button type="button" onClick={this.handleEditEvent.bind(this)} class="btn btn-sm btn-success">save</button>
-                    </td>
-                </tr>                               
+    render() {
+        return (
+            <tr>
+                <td>
+                    <div class = "form-group">
+                        <input onChange={this.handleChange.bind(this) } class="form-control" value ={this.props.partialEditTodo}/>
+                    </div>
+                </td>
+                <td> {this.props.item.status}</td>
+                <td>
+                    <div class="btn-group">
+                        <button type="button" onClick={this.handleDeleteEvent.bind(this) } class="btn btn-sm btn-danger">delete </button><span> </span>
+                        <button type="button" onClick={this.handleEditEvent.bind(this) } class="btn btn-sm btn-success">save</button>
+                    </div>
+                </td>
+            </tr>
         )
     }
 }
